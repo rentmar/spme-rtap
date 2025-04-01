0c58869e78239c3ee26d0baabb40ab8bff57e82b
marcelo <MRolqueza@gmail.com>
2025-04-01 17:08:28 -0400

    Primera Migracion

--- app/Database/Migrations/2025-04-01-153828_AddPlanPei.php
+++ app/Database/Migrations/2025-04-01-153828_AddPlanPei.php
@@ -9,10 +9,26 @@ class AddPlanPei extends Migration
     public function up()
     {
         //
+        $this->forge->addField([
+            'idpei' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name_pei' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+                'null' => false
+            ],
+        ]);
+        $this->forge->addPrimaryKey('idpei');
+        $this->forge->createTable('pei');
     }
 
     public function down()
     {
         //
+        $this->forge->dropTable('pei');
     }
 }
